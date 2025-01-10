6a9579082b6ceef7d62a19704763dd4d9e52d354
swagfather808 <rkringor@gmail.com>
2025-01-10 09:03:28 -1000

    Added some things to try see if it runs

--- App.js
+++ App.js
@@ -1,14 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, Buttons} from 'react-native';
 import Colors from './tabs/colors'
-import mainPage from './tabs/mainPage'
+import MainPage from './tabs/mainPage';
 let screen = <mainPage/>
 
 export default function App() {
   return (
-    <View>
-      {screen}
-    </View>
+    <MainPage></MainPage>
   );
 }
 
